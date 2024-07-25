ea9fc94928ece6e358db84766a89949b6ec02d25
Christan Shane Plaza <christanshaneplaza@gmail.com>
2024-07-25 17:30:46 +0800

    Initial Commit

---
+++ SwiftUI_Tasks/SwiftUI_TasksApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TasksApp.swift
+//  SwiftUI_Tasks
+//
+//  Created by Sun Asterisk Philippines on 7/25/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TasksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
